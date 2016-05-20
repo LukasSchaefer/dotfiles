3d82591bf0aae4375f2eec5849546951ced4a303
Lukas Schaefer <luki.schaefer96@gmail.com>
2016-05-21 01:20:21 +0200

    Reorganized scripts

---
+++ functions.zsh
@@ -0,0 +1,132 @@
+# Web function to open Websites
+web(){
+	if [ "$1" = "reddit" ]; then
+		if [ "$2" = "headphones" ]; then
+			open 'http://www.reddit.com/r/headphones/'
+			return
+		fi
+		if [ "$2" = "headphone" ]; then
+			open 'http://www.reddit.com/r/headphones/'
+			return
+		fi
+		if [ "$2" = "lol" ]; then
+			open 'http://www.reddit.com/r/leagueoflegends/'
+			return
+		fi
+		if [ "$2" = "csgo" ]; then
+			open 'http://www.reddit.com/r/globaloffensive/'
+			return
+		fi
+		if [ "$2" = "mechanicalkeyboards" ]; then
+			open 'http://www.reddit.com/r/mechanicalkeyboards/'
+			return
+		fi
+		if [ "$2" = "mechs" ]; then
+			open 'http://www.reddit.com/r/mechanicalkeyboards/'
+			return
+		fi
+		if [ "$2" = "keyboard" ]; then
+			open 'http://www.reddit.com/r/mechanicalkeyboards/'
+			return
+		fi
+		if [ "$2" = "keyboards" ]; then
+			open 'http://www.reddit.com/r/mechanicalkeyboards/'
+			return
+		fi
+		open 'http://www.reddit.com/r/Dashboard/'
+		return
+	fi
+
+	if [ "$1" = "youtube" ]; then
+		if [ "$2" = "watchlater" ]; then
+			open 'https://www.youtube.com/playlist?list=WL'
+			return
+		fi
+		if [ "$2" = "wl" ]; then
+			open 'https://www.youtube.com/playlist?list=WL'
+			return
+		fi
+		open 'https://www.youtube.com/?gl=DE&hl=de'
+		return
+	fi
+
+# Still to fill!!!!
+# _____________________________________________________________________________________________
+	if [ "$1" = "hlcv" ] || [ "$1" = "highlevel" ]; then
+        open 'http://www.mpi-inf.mpg.de/departments/computer-vision-and-multimodal-computing/teaching/courses/high-level-computer-vision/'
+	fi
+
+	if [ "$1" = "hci" ] || [ "$1" = "hcidev" ]; then
+        open 'http://umtl.cs.uni-saarland.de/index.php?id=288'
+	fi
+
+    if [ "$1" = "netflix" ]; then
+		open 'http://www.netflix.com'
+		return
+	fi
+
+	if [ "$1" = "gh" ] || [ "$1" = "github" ]; then
+		open 'https://github.com/LukasStudent'
+		return
+	fi
+
+    if [ "$1" = "twitch" ]; then
+		open 'http://www.twitch.tv'
+		return
+	fi
+
+	echo "invalid input after function call"
+}
+
+# web_search from terminal
+alias bing='web_search bing'
+alias google='web_search google'
+alias yahoo='web_search yahoo'
+alias ddg='web_search duckduckgo'
+alias yandex='web_search yandex'
+alias github='web_search github'
+alias baidu='web_search baidu'
+alias ecosia='web_search ecosia'
+#add your own !bang searches here
+alias wiki='web_search duckduckgo \!w'
+alias news='web_search duckduckgo \!n'
+alias youtube='web_search duckduckgo \!yt'
+alias map='web_search duckduckgo \!m'
+alias image='web_search duckduckgo \!i'
+alias ducky='web_search duckduckgo \!'
+
+function web_search() {
+  emulate -L zsh
+
+  # define search engine URLS
+  typeset -A urls
+  urls=(
+    google      "https://www.google.com/search?q="
+    bing        "https://www.bing.com/search?q="
+    yahoo       "https://search.yahoo.com/search?p="
+    duckduckgo  "https://www.duckduckgo.com/?q="
+    yandex      "https://yandex.ru/yandsearch?text="
+    github      "https://github.com/search?q="
+    baidu       "https://www.baidu.com/s?wd="
+    ecosia      "https://www.ecosia.org/search?q="
+  )
+
+  # check whether the search engine is supported
+  if [[ -z "$urls[$1]" ]]; then
+    echo "Search engine $1 not supported."
+    return 1
+  fi
+
+  # search or go to main page depending on number of arguments passed
+  if [[ $# -gt 1 ]]; then
+    # build search url:
+    # join arguments passed with '+', then append to search engine URL
+    url="${urls[$1]}${(j:+:)@[2,-1]}"
+  else
+    # build main page url:
+    # split by '/', then rejoin protocol (1) and domain (2) parts with '//'
+    url="${(j://:)${(s:/:)urls[$1]}[1,2]}"
+  fi
+
+  open "$url"
+}
