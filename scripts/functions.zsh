690e5950eb5bfc926dbc569089b44762f93f8de8
Lukas Schaefer <luki.schaefer96@gmail.com>
2019-03-04 13:36:47 +0000

    only show instruction/ help line of pdfcompress script if argument number was invalid

--- scripts/functions.zsh
+++ scripts/functions.zsh
@@ -164,7 +164,7 @@ livestream(){
 EOF
 }
 
-pdfmerge(){
+pdfmerge() {
     if ! [ "$#" -ge 3 ]; then
         echo "ERROR: The script needs at least 3 arguments: <output.pdf> <input1.pdf> <input2.pdf> <...>"
         return
@@ -187,4 +187,18 @@ pdfmerge(){
     gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress -sOutputFile=$1 ${@:2}
     echo "Merge successfull!"
     return
+}
+
+compresspdf() {
+    if ! [ "$#" -ge 2 ]; then
+        echo 'Usage: compresspdf [input file] [output file] [screen|ebook|printer|prepress]'
+        return
+    fi
+
+    if [ "$#" -ge 4 ]; then
+        echo 'Usage: compresspdf [input file] [output file] [screen|ebook|printer|prepress]'
+        return
+    fi
+
+    gs -sDEVICE=pdfwrite -dNOPAUSE -dQUIET -dBATCH -dPDFSETTINGS=/${3:-"screen"} -dCompatibilityLevel=1.4 -sOutputFile="$2" "$1"
 }
